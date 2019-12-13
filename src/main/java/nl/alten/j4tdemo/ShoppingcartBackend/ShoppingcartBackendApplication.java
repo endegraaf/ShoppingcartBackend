e781013a0b78a274602bf3af845a80cef746b418
Eric de Graaf <endegraaf@gmail.com>
2019-12-13 10:07:06 +0100

    initial commit

---
+++ src/main/java/nl/alten/j4tdemo/ShoppingcartBackend/ShoppingcartBackendApplication.java
@@ -0,0 +1,13 @@
+package nl.alten.j4tdemo.ShoppingcartBackend;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class ShoppingcartBackendApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(ShoppingcartBackendApplication.class, args);
+	}
+
+}
---
+++ src/test/java/nl/alten/j4tdemo/ShoppingcartBackend/ShoppingcartBackendApplicationTests.java
@@ -0,0 +1,13 @@
+package nl.alten.j4tdemo.ShoppingcartBackend;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class ShoppingcartBackendApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
